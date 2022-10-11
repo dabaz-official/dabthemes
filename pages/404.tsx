7827b14124ffc385de27577e0a1b74215af8b999
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-10-12 05:16:09 +0800

    Add 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -17,66 +17,20 @@ export default function error() {
       <main>
         <Masthead />
         <section className='px-8 mx-auto max-w-7xl my-12'>
-          <h1 className='text-2xl font-semibold text-blue-600 dark:text-blue-400'>
-            Introduction
+          <h1 className='text-3xl text-center font-semibold mt-12 mb-8'>
+            Oops, not found
           </h1>
-          <p className='mb-2'>
-            This website only provides some of the themes of several IDE and shells. These themes are my favorite ones and I want to share them with y&apos;all. And that is the reason why I build DabThemes.
+          <p className='mb-2 text-center'>
+            The page you are looking for may be removed...
           </p>
-          <h1 className='text-2xl font-semibold text-blue-600 dark:text-blue-400'>
-            Contact
-          </h1>
-          <p className='mb-2'>
-            Here are some of my contacts:
-          </p>
-          <ul className='mb-2 list-disc'>
-            <li>
-              Twitter:{' '}
-              <a
-                href='https://twitter.com/dabaz_official'
-                className='underline underline-offset-2 decoration-blue-500 hover:text-blue-600 hover:decoration-blue-600 dark:hover:text-blue-400 dark:hover:decoration-blue-400 transition duration-200'
-              >
-                @dabaz_official
-              </a>
-            </li>
-            <li>
-              Instagram:{' '}
-              <a
-                href='https://instagram.com/dabaz_official'
-                className='underline underline-offset-2 decoration-blue-500 hover:text-blue-600 hover:decoration-blue-600 dark:hover:text-blue-400 dark:hover:decoration-blue-400 transition duration-200'
-              >
-                @dabaz_official
-              </a>
-            </li>
-            <li>
-              GitHub:{' '}
-              <a
-                href='https://github.com/dabaz-official'
-                className='underline underline-offset-2 decoration-blue-500 hover:text-blue-600 hover:decoration-blue-600 dark:hover:text-blue-400 dark:hover:decoration-blue-400 transition duration-200'
-              >
-                @dabaz-official
-              </a>
-            </li>
-            <li>
-              Email:{' '}
-              <a
-                href='mailto:dabaz@dabazofficial.com'
-                className='underline underline-offset-2 decoration-blue-500 hover:text-blue-600 hover:decoration-blue-600 dark:hover:text-blue-400 dark:hover:decoration-blue-400 transition duration-200'
-              >
-                dabaz@dabazofficial.com
-              </a>
-            </li>
-          </ul>
-          <p className='mb-2'>
-            If you want to send a direct message, just go to my{' '}
+          <div className='text-center py-4'>
             <a
-              href='https://dabaz.vercel.app/contact'
-              className='underline underline-offset-2 decoration-blue-500 hover:text-blue-600 hover:decoration-blue-600 dark:hover:text-blue-400 dark:hover:decoration-blue-400 transition duration-200'
+              className='inline-flex items-center gap-1 underline underline-offset-2 decoration-blue-500 hover:decoration-blue-600 dark:hover:decoration-blue-400 transition duration'
+              href='/'
             >
-              contact page
+              Go to Index
             </a>
-            , fill in the contact form and click the &quot;Submit&quot; button. I will reply ASAP.
-          </p>
+          </div>
         </section>
       </main>
 
