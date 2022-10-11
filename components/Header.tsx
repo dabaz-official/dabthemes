c03398b9165077964cb04a4dad0ce0d0d1e72a4e
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-10-12 02:46:22 +0800

    Add header component

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react'
+import Logo from './Logo'
+
+export default function Header() {
+  return (
+    <header className='relative w-full p-2 z-20 bg-zinc-50 dark:bg-zinc-900'>
+      <div className='mx-auto max-w-7xl'>
+        <nav className='flex items-center gap-3 text-base'>
+          <a href='/' className='group'>
+            <h2 className='font-bold p-2 text-xl text-zinc-900 dark:text-white'>
+              <img src='/images/global/logo.svg' alt='Logo' className='w-5 inline-block group-hover:rotate-[30deg] transition duration-200 mr-2 mb-1' />
+              DabThemes
+            </h2>
+          </a>
+          <div className='items-center gap-6 hidden md:block'>
+            <a href='https://dabaz.vercel.app/contact' className='text-zinc-900 dark:text-zinc-100'>
+              Contact
+            </a>
+          </div>
+          <div className='flex-1'></div>
+        </nav>
+      </div>
+    </header>
+  )
+}
