8636657eb1985aafc9629290df7fe4993606e304
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-10-12 05:16:41 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ const today = new Date()
 export default function Footer() {
   return (
     <footer className='text-zinc-500 p-2 text-center'>
-      &copy; {today.getFullYear()}
+      &copy; {today.getFullYear()}{' '}
       <a href='https://dabaz.vercel.app'>DabAZ</a>. All rights reserved.
     </footer>
   )
